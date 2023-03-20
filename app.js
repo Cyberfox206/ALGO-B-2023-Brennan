dbfd8f5839850cc30bdc973308f931ec4f6a827a
Cyberfox206 <bbrontesi@edenschool.fr>
2023-03-20 17:00:38 +0100

    last commit for blank intero

---
+++ app.js
@@ -0,0 +1,147 @@
+
+// function ex1() {
+//     let stvide = "";
+//     for (let i = 1; i < 11; i++) {
+//         stvide = stvide + i;
+//     }
+//     console.log(stvide);
+// }
+// ex1();
+
+// function ex2() {
+//     let stvide = "";
+//     for (let i = 1; i < 11; i++) {
+//         stvide = stvide + ` ${i}`;
+//     }
+//     console.log(stvide);
+// }
+// ex2();
+
+// function ex3() {
+//     let stvide = "";
+//     for (let i = 1; i < 101; i++) {
+//         if (i % 2 === 1) {
+//             stvide = stvide + ` ${i}`;
+//         }
+//     }
+//     console.log(stvide);
+// }
+// ex3();
+
+// function ex4() {
+//     let st = "";
+//     for (let i = 0; i < 11; i++) {
+//         st += `${i} x 7 = ${i * 7} `
+//     }
+//     console.log(st)
+// }
+// ex4();
+
+// function ex5() {
+//     let st = "";
+//     for (let m = 0; m < 11; m++) {
+//         for (let n = 0; n < 11; n++) {
+//             if (n === 11) {
+//                 n = 0;
+//             }
+//             st += `${n} x ${m} = ${n * m} `
+//         }
+//     }
+//     console.log(st);
+// }
+// ex5();
+
+
+// function ex6() {
+//     let tmp = 0;
+//     for (let i = 0; i < 11; i++) {
+//         tmp = tmp + i;
+//     }
+//     console.log(tmp)
+// }
+// ex6();
+
+
+// exo7
+
+
+
+// function ex8() {
+//     let tmp = 10;
+//     for (let i = 10; i < 33; i++) {
+//         tmp = tmp + i;
+//     }
+//     console.log(tmp)
+// }
+// ex8();
+
+// function ex9(c) {
+//     let result = 0;
+//     result = (c * 9 / 5) + 32
+//     console.log(result)
+// }
+// ex9(10);
+
+// function exo10(tab) {
+//     let result = 0;
+//     for (let i = 0; i < tab.length; i++) {
+//         result = result + tab[i];
+//     }
+//     console.log(result)
+// }
+// exo10([10, 10]);
+
+// function exo11(tab) {
+//     let moyen = 0;
+//     for (let i = 0; i < tab.length; i++) {
+//         moyen = moyen + tab[i]
+//     }
+//     moyen = moyen / tab.length;
+//     console.log(moyen)
+// }
+// exo11([10, 10, 15]);
+
+// function exo12(tab) {
+//     let newtab = [];
+//     for (let i = 0; i < tab.length; i++) {
+//         if (tab[i] < 0) {
+//             newtab.push(tab[i]);
+//         }
+//     }
+//     console.log(newtab);
+// }
+// exo12([-1, -5, 10, 20, -14]);
+
+// function exo13(tab) {
+//     let result = tab[0];
+//     for (let i = 0; i < tab.length; i++) {
+//         if (result < tab[i]) {
+//             result = tab[i];
+//         }
+//     }
+//     console.log(result)
+// }
+// exo13([10, 100, 15, 20, 13, 200]);
+
+
+// function ex14() {}
+// ex14();
+
+
+// function ex15(n) {
+//     let a = n.toString();
+//     let result = 0;
+//     for (let i = 0; i < a.length; i++) {
+//         result = result + parseInt(a[i])
+//     }
+//     console.log(result);
+// }
+// ex15(123)
+
+// function ex16(tab) {
+// }
+// ex16([1, 5])
+
+
+
+
